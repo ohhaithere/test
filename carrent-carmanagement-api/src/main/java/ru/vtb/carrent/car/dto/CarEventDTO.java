e0100e78ef1df9f7882396e038d0490c917e0afb
Valaiantsin Charkashyn <valiantsin_charkashyn@epam.com>
2018-05-31 15:40:29 +0300

    Remove apimodelprops
    Issue: VTBDBOTND-409
    Change-Id: Ic63a60e0f07a490555ec2adf3fdf1ee7ede84215

--- carrent-carmanagement-api/src/main/java/ru/vtb/carrent/car/dto/CarEventDTO.java
+++ carrent-carmanagement-api/src/main/java/ru/vtb/carrent/car/dto/CarEventDTO.java
@@ -1,6 +1,5 @@
 package ru.vtb.carrent.car.dto;
 
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -20,51 +19,35 @@ import java.util.Date;
 @Accessors(chain = true)
 public class CarEventDTO {
 
-    @ApiModelProperty(value = "Идентификатор", example = "1")
     private Long id;
 
-    @ApiModelProperty(value = "Марка автомобиля", example = "Lada Vesta")
     private String model;
 
-    @ApiModelProperty(value = "Государственный номер", example = "с123тт-54")
     private String regNumber;
 
-    @ApiModelProperty(value = "Год выпуска")
     private Date dateOfManufacture;
 
-    @ApiModelProperty(value = "Пробег на последнюю дату в статусе в наличии", example = "11")
     private int mileage;
 
-    @ApiModelProperty(value = "Дата последнего ТО")
     private Date dateOfLastCheck;
 
-    @ApiModelProperty(value = "Дата следуюшего ТО")
     private Date dateOfNextCheck;
 
-    @ApiModelProperty(value = "Текуший статус", example = "в наличии")
     private String currentStatus;
 
-    @ApiModelProperty(value = "Дата перехода в текущий статус")
     private Date dateOfCurrentStatus;
 
-    @ApiModelProperty(value = "Следующий статус", example = "в наличии")
     private String nextStatus;
 
-    @ApiModelProperty(value = "Плановая дата следуюшего статуса")
     private Date dateOfNextStatus;
 
-    @ApiModelProperty(value = "Локация автомобиля")
     private LocationDto location;
 
-    @ApiModelProperty(value = "Дата события")
     private Date eventDate;
 
-    @ApiModelProperty(value = "Номер события", example = "1")
     private long sequenceId;
 
-    @ApiModelProperty(value = "Тип события", example = "CREATE")
     private String eventType;
 
-    @ApiModelProperty(value = "Локализованный тип события", example = "Создание")
     private String eventTypeDisplayName;
 }
