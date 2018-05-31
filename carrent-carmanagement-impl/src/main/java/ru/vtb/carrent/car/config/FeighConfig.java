1fb594fe947a3dc7097fe91f2fdbecc9f47c8bbc
Valaiantsin Charkashyn <valiantsin_charkashyn@epam.com>
2018-05-31 17:54:28 +0300

    Added feigh client
    Issue: VTBDBOTND-409
    Change-Id: Id00bf12e7b8d674963c274b931419c50a2acc483

---
+++ carrent-carmanagement-impl/src/main/java/ru/vtb/carrent/car/config/FeighConfig.java
@@ -0,0 +1,19 @@
+/*
+ * VTB Group. Do not reproduce without permission in writing.
+ * Copyright (c) 2017 VTB Group. All rights reserved.
+ */
+
+package ru.vtb.carrent.car.config;
+
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Feigh config.
+ *
+ * @author Valiantsin_Charkashy
+ */
+@Configuration
+@EnableFeignClients
+public class FeighConfig {
+}
---
+++ carrent-carmanagement-impl/src/main/java/ru/vtb/carrent/car/resource/LocationResourceClient.java
@@ -0,0 +1,35 @@
+/*
+ * VTB Group. Do not reproduce without permission in writing.
+ * Copyright (c) 2017 VTB Group. All rights reserved.
+ */
+package ru.vtb.carrent.car.resource;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import ru.vtb.carrent.organization.dto.LocationDto;
+import ru.vtb.carrent.organization.resource.LocationResource;
+
+import java.util.Collections;
+import java.util.List;
+
+
+/**
+ * Client to fetch location
+ *
+ * @author Valiantsin_Charkashy
+ */
+@FeignClient(name = "carrent-organization", fallback = LocationResourceClient.LocationResourceClientFallback.class)
+public interface LocationResourceClient extends LocationResource {
+
+    class LocationResourceClientFallback implements LocationResourceClient {
+        @Override
+        public List<LocationDto> getLocations() {
+            return Collections.emptyList();
+        }
+
+        @Override
+        public LocationDto getLocation(@PathVariable("id") Long id) {
+            return null;
+        }
+    }
+}
